a9c318ebcfd2babaee6962889dd260276efbfa71
Dustin Schultz <schultz.dustin@gmail.com>
2019-12-01 09:10:58 -0700

    Initial commit

---
+++ src/main/java/io/schultz/dustin/todoapp/TodoAppApplication.java
@@ -0,0 +1,13 @@
+package io.schultz.dustin.todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/schultz/dustin/todoapp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.schultz.dustin.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
